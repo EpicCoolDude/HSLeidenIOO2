f7772f9fbee4cd04fe836d90b604db7027aa8f11
epiccooldude <epiccooldude>
2022-02-11 12:40:16 +0100

    lees en schrijf steden opdracht afgerond

--- src/week1/Opdrachten/LeesStedenBestandLezenSchrijvenApp.java
+++
@@ -1,9 +0,0 @@
-package week1.Opdrachten;
-
-public class LeesStedenBestandLezenSchrijvenApp {
-
-    public static void main(String[] args) {
-
-        LeesSteden.printStedenUitBestand("/home/tim/IdeaProjects/HSLeidenIOO2/src/week1/test/plaatsen.txt");
-    }
-}
